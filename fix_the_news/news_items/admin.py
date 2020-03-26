5a245d14774e9daf4f7c991d31b5971a73991cb9
roy <royhanley8@gmail.com>
2020-03-26 21:06:04 +0000

    Admin logic added for news items

--- fix_the_news/news_items/admin.py
+++ fix_the_news/news_items/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from fix_the_news.news_items import models
+
+
+class NewsItemAdmin(admin.ModelAdmin):
+    ordering = (
+        "date_created",
+    )
+    search_fields = (
+        "title",
+        "topic__title"
+        "type__title",
+        "user__email",
+        "user__username",
+        "url",
+        "category__title",
+    )
+
+
+class NewsTypeAdmin(admin.ModelAdmin):
+    ordering = (
+        "date_created",
+    )
+    search_fields = (
+        "title",
+    )
+
+
+admin.site.register(models.NewsItem, NewsItemAdmin)
+admin.site.register(models.NewsType, NewsTypeAdmin)
