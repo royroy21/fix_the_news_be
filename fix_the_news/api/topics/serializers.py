da97c8346fe9f8bd9d6bac7db8ce330c50b245fe
roy <royhanley8@gmail.com>
2020-04-08 16:45:01 +0100

    Added news_items_count field for TopicSerializer

--- fix_the_news/api/topics/serializers.py
+++ fix_the_news/api/topics/serializers.py
@@ -18,6 +18,7 @@ class CategorySerializer(serializers.ModelSerializer):
 class TopicSerializer(serializers.ModelSerializer):
 
     categories = CategorySerializer(many=True)
+    news_items_count = serializers.SerializerMethodField()
     top_news_items = serializers.SerializerMethodField()
 
     class Meta:
@@ -25,11 +26,20 @@ class TopicSerializer(serializers.ModelSerializer):
         fields = (
             "categories",
             "id",
+            "news_items_count",
             "title",
             "top_news_items",
             "user",
         )
         read_only_fields = fields
+
+    def get_news_items_count(self, obj):
+        return {
+            key: obj.news_items.get_active().filter(
+                category__type=key).count()
+            for key
+            in models.Category.ALL_TYPE_CHOICES
+        }
 
     def get_top_news_items(self, obj):
         from fix_the_news.api.news_items.serializers import NewsItemSerializer
