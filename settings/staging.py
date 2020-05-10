a30bc24f6b2e1b5609bdbbe6cae7bef4a825d3e9
roy <royhanley8@gmail.com>
2020-05-10 17:04:18 +0100

    Added settings file for staging

---
+++ settings/staging.py
@@ -0,0 +1,18 @@
+from .base import *
+
+CORS_ORIGIN_WHITELIST = (
+    os.environ["WEB_APP_URL"],
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["DATABASE_NAME"],
+        'USER': os.environ["DATABASE_USER"],
+        'PASSWORD': os.environ["DATABASE_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],
+        'PORT': os.environ["DATABASE_PORT"],
+    }
+}
+
+# TODO - media to be stored on s3 bucket
