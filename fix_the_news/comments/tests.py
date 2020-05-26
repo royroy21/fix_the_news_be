14e0787f800a1413406577255b5ecd5d46a0a74d
roy <royhanley8@gmail.com>
2020-05-26 18:17:47 +0100

    Refactor comment model tests + added test_add_by_content_object

--- fix_the_news/comments/tests.py
+++ fix_the_news/comments/tests.py
@@ -8,20 +8,48 @@ from fix_the_news.news_items import models as news_items_models
 
 class TestCommentGenericRelations(TestCase):
 
-    def test_adding_generic_relation(self):
-        comment_text = 'this is a comment'
-        comment = G(models.Comment, text=comment_text)
-        news_item_title = 'this is a news item'
-        news_item = G(news_items_models.NewsItem, title=news_item_title)
-        news_item.comments.add(comment)
+    def setUp(self):
+        self.comment_text = 'this is a comment'
+        self.comment = G(
+            models.Comment,
+            text=self.comment_text,
+        )
+        self.news_item_title = 'this is a news item'
+        self.news_item = G(
+            news_items_models.NewsItem,
+            title=self.news_item_title,
+        )
 
-        self.assertEqual(news_item.comments.first().text, comment_text)
-        self.assertEqual(comment.content_object.title, news_item_title)
+    def test_add_by_generic_relation(self):
+        self.news_item.comments.add(self.comment)
+
+        self.assertEqual(
+            self.news_item.comments.first().text,
+            self.comment_text,
+        )
+        self.assertEqual(
+            self.comment.content_object.title,
+            self.news_item_title,
+        )
+
+    def test_add_by_content_object(self):
+        self.comment.content_object = self.news_item
+        self.comment.save()
+
+        self.assertEqual(
+            self.news_item.comments.first().text,
+            self.comment_text,
+        )
+        self.assertEqual(
+            self.comment.content_object.title,
+            self.news_item_title,
+        )
 
     def test_add_comments_to_a_comment(self):
-        comment = G(models.Comment)
-        comment_text = 'this is a comment'
-        another_comment = G(models.Comment, text=comment_text)
-        comment.comments.add(another_comment)
+        another_comment = G(models.Comment, text=self.comment_text)
+        self.comment.comments.add(another_comment)
 
-        self.assertEqual(comment.comments.first().text, comment_text)
+        self.assertEqual(
+            self.comment.comments.first().text,
+            self.comment_text,
+        )
