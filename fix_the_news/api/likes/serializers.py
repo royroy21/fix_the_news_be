2966c1a5df388168f807bcd1fb9a72fa2f9063e4
roy <royhanley8@gmail.com>
2020-06-18 22:30:55 +0100

    Added comment at LikeSerializer

--- fix_the_news/api/likes/serializers.py
+++ fix_the_news/api/likes/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from fix_the_news.likes import models
 
 
+# TODO - do not allow a user to like an object more than once
 class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
