2406010d690b86e446da8d2f3a07df4d046f8180
roy <royhanley8@gmail.com>
2020-06-18 23:19:09 +0100

    Added enforce_object_not_already_liked and enforce_only_one_object_type_is_being_liked validation to LikeSerializer

--- fix_the_news/api/likes/serializers.py
+++ fix_the_news/api/likes/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from fix_the_news.likes import models
 
 
-# TODO - do not allow a user to like an object more than once
 class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -19,3 +20,41 @@ class LikeSerializer(serializers.ModelSerializer):
             'id',
             'date_created',
         )
+
+    def validate(self, attrs):
+        self.enforce_object_not_already_liked(attrs)
+        self.enforce_only_one_object_type_is_being_liked(attrs)
+        return super().validate(attrs)
+
+    likable_objects = [
+        "comment",
+        "news_item",
+        "topic",
+    ]
+
+    def enforce_object_not_already_liked(self, attrs):
+        for object_name in self.likable_objects:
+            obj = attrs.get(object_name)
+            if not obj:
+                continue
+            query = obj.likes.filter(**{
+                object_name: obj,
+                "user": self.context["request"].user,
+            })
+            if query.exists():
+                raise ValidationError({
+                    "non_field_errors": ["You have liked this already"],
+                })
+
+    def enforce_only_one_object_type_is_being_liked(self, attrs):
+        objects = [
+            attrs[object_name]
+            for object_name
+            in self.likable_objects
+            if attrs.get(object_name)
+        ]
+        if len(objects) > 1:
+            raise ValidationError({
+                "non_field_errors": ["You can only like one "
+                                     "object type at a time"],
+            })
--- fix_the_news/api/likes/tests.py
+++ fix_the_news/api/likes/tests.py
@@ -4,7 +4,9 @@ from django_dynamic_fixture import G
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from fix_the_news.comments import models as comments_models
 from fix_the_news.likes import models
+from fix_the_news.topics import models as topics_models
 from fix_the_news.users import models as users_models
 
 
@@ -26,3 +28,26 @@ class TestLikeViewSet(TestCase):
         endpoint = reverse('like-detail', kwargs={'pk': like.id})
         response = self.authenticated_client.delete(endpoint)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_like_object_more_than_once(self):
+        comment = G(comments_models.Comment)
+        G(models.Like, user=self.user, comment=comment)
+        endpoint = reverse("like-list")
+        data = {
+            "comment": comment.id,
+        }
+        response = self.authenticated_client.post(endpoint, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(comment.likes.count(), 1)
+
+    def test_more_than_one_object_is_being_liked(self):
+        comment = G(comments_models.Comment)
+        topic = G(topics_models.Topic)
+        endpoint = reverse("like-list")
+        data = {
+            "comment": comment.id,
+            "topic": topic.id,
+        }
+        response = self.authenticated_client.post(endpoint, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(comment.likes.count(), 0)
