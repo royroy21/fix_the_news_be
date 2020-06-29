b80bb4bb706b23d6fc58d0086fecfc7991d9fccc
roy <royhanley8@gmail.com>
2020-06-29 22:11:11 +0100

    Changes to NewsItemURLService: Retry request if connection error + fake headers so it looks like request came from a browser

--- fix_the_news/news_items/services.py
+++ fix_the_news/news_items/services.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import requests
 from requests import exceptions as requests_exceptions
@@ -9,18 +10,26 @@ logger = logging.getLogger(__name__)
 
 class NewsItemURLService:
 
-    PREPEND_HTTP = 'http://'
-    PREPEND_HTTPS = 'https://'
+    PREPEND_HTTP = "http://"
+    PREPEND_HTTPS = "https://"
+    MAX_RETRIES = 3
+    SECONDS_TO_WAIT_BEFORE_RETRY = 2
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/83.0.4103.61 Safari/537.36",
+    }
 
     def parse_and_validate(self, url):
         """ Returns parsed url and validation error if error exists """
-        https_url = self.parse(url)
+        cleaned_url = url.strip()
+        https_url = self.parse(cleaned_url)
         error = self.validate(https_url)
         if error:
-            http_url = self.parse(url, force_http=True)
+            http_url = self.parse(cleaned_url, force_http=True)
             error = self.validate(http_url)
             if error:
-                return url, error
+                return cleaned_url, error
             return http_url, None
         return https_url, None
 
@@ -34,29 +43,34 @@ class NewsItemURLService:
 
         return f"{self.PREPEND_HTTPS}{url}"
 
-    def validate(self, url):
+    def validate(self, url, retry=1):
         """ Returns an error if URL is not valid otherwise None """
+        exception_message = "Error validating URL:%s error:%s"
         try:
-            response = requests.get(url)
+            response = requests.get(url, headers=self.HEADERS)
         except (
             requests_exceptions.ConnectionError,
             requests_exceptions.ConnectTimeout,
-            requests_exceptions.SSLError,
         ) as error:
-            logger.error(
-                'Error validating URL:%s error:%s',
-                url,
-                error,
-            )
-            return error
+            if retry <= self.MAX_RETRIES:
+                logger.debug(
+                    "Error validating URL:%s error:%s retry:%s",
+                    url, error, retry,
+                )
+                time.sleep(self.SECONDS_TO_WAIT_BEFORE_RETRY)
+                return self.validate(url, retry + 1)
+            else:
+                logger.error(exception_message, url, error)
+                return "Connection error"
+        except requests_exceptions.SSLError as error:
+            logger.error(exception_message, url, error)
+            return "Site certificate not valid"
 
         if not response.ok:
             logger.error(
-                'Error validating URL:%s status_code:%s response:%s',
-                url,
-                response.status_code,
-                response.text,
+                "Error validating URL:%s status_code:%s response:%s",
+                url, response.status_code, response.text,
             )
-            return 'Unexpected error'
+            return response.reason
 
         return None
