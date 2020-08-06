68a1b3bad5678c1a3ffecee18c3b38b62cd41c20
roy <royhanley8@gmail.com>
2020-08-06 14:13:24 +0100

    Remove logging for rabbit image upload

--- fix_the_news/api/users/serializers.py
+++ fix_the_news/api/users/serializers.py
@@ -1,5 +1,3 @@
-import logging
-
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.db.transaction import on_commit
@@ -12,7 +10,6 @@ from rest_framework.serializers import CharField
 
 from fix_the_news.users.tasks import create_avatar_thumbnail
 
-logger = logging.getLogger(__name__)
 User = get_user_model()
 
 
@@ -40,12 +37,8 @@ class CurrentUserSerializer(DjoserUserSerializer):
 
     def save(self, **kwargs):
         user = super().save(**kwargs)
-        try:
-            if "avatar" in self.validated_data:
-                on_commit(lambda: create_avatar_thumbnail.delay(user.id))
-        except Exception as error:
-            logger.error('RABBIT IMAGE UPLOAD ERROR %s', error)
-
+        if "avatar" in self.validated_data:
+            on_commit(lambda: create_avatar_thumbnail.delay(user.id))
         return user
 
 
