3fc9c36d3856df10faad4a01409f3054e32694fa
Roy Hanley <royhanley8@gmail.com>
2020-08-12 23:56:04 +0100

    At inactivate_communications signal update all users has_viewed_daily_communication field

--- fix_the_news/communications/signals.py
+++ fix_the_news/communications/signals.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
@@ -6,4 +7,9 @@ from fix_the_news.communications import models
 
 @receiver(pre_save, sender=models.Communication)
 def inactivate_communications(sender, instance, **kwargs):
-    sender.objects.filter(type=instance.type).update(active=False)
+    communication_type = instance.type
+    sender.objects.filter(type=communication_type).update(active=False)
+
+    if communication_type == models.Communication.DAILY:
+        get_user_model().objects.all()\
+            .update(has_viewed_daily_communication=False)
